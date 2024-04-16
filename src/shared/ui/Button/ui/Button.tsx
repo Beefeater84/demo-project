36713c521f78078105ea27e77e75bcfa033b95a1
Miaseodov Tony <myasoedov.blog@gmail.com>
2024-04-16 09:39:57 +0300

    create button UI

---
+++ src/shared/ui/Button/ui/Button.tsx
@@ -0,0 +1,17 @@
+import styles from "./Button.module.css";
+import {ButtonHTMLAttributes, FC, ReactNode} from "react";
+import classNames from "classnames";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string
+    children?: ReactNode
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const {className, children} = props;
+    return (
+        <button className={classNames(styles.Button, className)}>
+            {children}
+        </button>
+    )
+}
--- src/widget/theme-switcher/ui/ThemeSwitcher.tsx
+++ src/widget/theme-switcher/ui/ThemeSwitcher.tsx
@@ -4,6 +4,7 @@ import classNames from "classnames";
 import DartThemeIcon from "@/shared/assets/icons/theme-dark.svg";
 import LightThemeIcon from "@/shared/assets/icons/theme-light.svg";
 import {ThemeContext, THEMES} from "@/app/providers/them-provider";
+import {Button} from "@/shared/ui/Button";
 
 
 interface ThemeSwitcherProps {
@@ -17,11 +18,11 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = (props) => {
     const {theme, toggleTheme} = useContext(ThemeContext)
 
     return (
-        <button
+        <Button
             className={classNames(styles.ThemeSwithcher, className)}
             onClick={toggleTheme}
         >
             {theme === THEMES.LIGHT ? <LightThemeIcon/> : <DartThemeIcon/>}
-        </button>
+        </Button>
     )
 }
